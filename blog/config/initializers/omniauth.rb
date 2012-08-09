459119e0ddbe6c32942087c3c2de3a1e75609faf
dfang <dfang@outlook.com>
2012-08-09 15:28:43 +0800

    add github key and secret , add authorize with github link

--- blog/config/initializers/omniauth.rb
+++ blog/config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider  :github,'4d2a1defa8700f396d2a','ecb44c6cfba6d082f8f6cd021a04f86fdbe65bba'
+  
+
+ #4d2a1defa8700f396d2a
+ #ecb44c6cfba6d082f8f6cd021a04f86fdbe65bba
 end
